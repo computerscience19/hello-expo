47413777f4096e313a9f706f881e41bd36b86285
ialab <110453745+computerscience19@users.noreply.github.com>
2024-01-24 15:36:44 +0900

    Update App.js

--- App.js
+++ App.js
@@ -1,20 +1,36 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { SafeAreaView, StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>oh god~!!</Text>
+      <View style = {styles.box1}><Text>안녕하세요 여러분</Text></View>
+      <View style = {styles.box2}><Text>동아리장을 맡은 김준수입니다.</Text></View>
       <StatusBar style="auto" />
     </View>
+  
   );
 }
 
 const styles = StyleSheet.create({
+
   container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    flex:1,
+    flexDirection:'row',
   },
+
+  box1: {
+    flex:1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor:'pink',
+  },
+
+  box2: {
+    flex:1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor:'white',
+  },
+
 });

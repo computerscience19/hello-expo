d1b0fd2ae7c65b860655819d28437f21689ace1e
ialab <110453745+computerscience19@users.noreply.github.com>
2024-02-26 16:24:47 +0900

    과제 두번째 커밋
    넘버보드 및 기타

--- App.tsx
+++ App.tsx
@@ -1,5 +1,5 @@
-import { StyleSheet, SafeAreaView, Dimensions, } from 'react-native';
-import { Box, Center, GluestackUIProvider, Image, ScrollView, Text, VStack, View } from '@gluestack-ui/themed';
+import { StyleSheet,  Dimensions, } from 'react-native';
+import { SafeAreaView, Box, Center, GluestackUIProvider, Image, ScrollView, Text, VStack, View } from '@gluestack-ui/themed';
 import { config } from '@gluestack-ui/config';
 import Swiper from 'react-native-swiper';
 import MySwiper from "./components/MySwiper";
@@ -8,18 +8,32 @@ import { QueryClient, QueryClientProvider, useQuery } from '@tanstack/react-quer
 import MediumCard from "./components/Card/MediumCard";
 import AppScreen from './screens/AppScreen';
 import CounterScreen from './screens/CounterScreen';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+import { ScreensParams } from './types';
+import { StatusBar } from 'expo-status-bar';
 
 
-
+const Stack = createStackNavigator<ScreensParams>();
 const queryClient = new QueryClient();
 
 export default function App() {
-  
   return (
-    
-    <CounterScreen />
-
-      
+    <QueryClientProvider client={queryClient}>
+      <GluestackUIProvider config={config}>
+        <SafeAreaView flex={1} bg='$backgroundDark950'>
+          <NavigationContainer>
+            <Stack.Navigator>
+              <Stack.Screen
+                name='Counter'
+                component={CounterScreen}
+                options={{ header: () => null }}
+              />
+            </Stack.Navigator>
+          </NavigationContainer>
+        </SafeAreaView>
+      </GluestackUIProvider>
+    </QueryClientProvider>
   );
 }
 
